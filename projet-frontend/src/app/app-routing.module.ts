29eb367dd94aa63073259d60dc622f05a9955985
AminataDieye <n.aminatadieye@gmail.com>
2019-09-04 14:28:35 +0000

    first commit

---
+++ projet-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AccueilComponent } from './component/accueil/accueil.component';
+import { LoginComponent } from './component/login/login.component';
+import { ArchivesComponent } from './component/archives/archives.component';
+import { EditionComponent } from './component/edition/edition.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { ProposerContenusComponent } from './component/proposer-contenus/proposer-contenus.component';
+import { AuthGuard } from './auth/auth.guard';
+
+const routes: Routes = [
+  //{ path: "accueil", component: AccueilComponent },
+  { path: "edition", component: EditionComponent },
+  { path: "archives", component: ArchivesComponent },
+  { path: "proposer-contenus", component: ProposerContenusComponent },
+  {
+    path: "accueil",
+    component: AccueilComponent,
+    canActivate: [AuthGuard]
+  },
+  { path: "contact", component: ContactComponent },
+  { path: "login", component: LoginComponent },
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  // { path: "**", component: PageNotFoundComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ projet-frontend/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { AccueilComponent } from './component/accueil/accueil.component';
+import { ArchivesComponent } from './component/archives/archives.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { LoginComponent } from './component/login/login.component';
+import { EditionComponent } from './component/edition/edition.component';
+import { ProposerContenusComponent } from './component/proposer-contenus/proposer-contenus.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AccueilComponent,
+    ArchivesComponent,
+    ContactComponent,
+    LoginComponent,
+    EditionComponent,
+    ProposerContenusComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
